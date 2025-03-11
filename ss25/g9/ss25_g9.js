3d77ac876814874ea2b06612068028b751d3d420
mtrangcp <haminhtrang545@gmail.com>
2025-03-11 21:55:21 +0700

    first commit

---
+++ ss25/g9/ss25_g9.js
@@ -0,0 +1,25 @@
+let users = ["ha@gmail.vn"];
+const emailRegex = /^[^\s@]+@[^\s@]+\.(com|vn)$/;
+
+const checkEmail = (email) => {
+    let text = "";
+    if (emailRegex.test(email)) {
+
+        if (!users.includes(email)) {
+            users.push(email);
+            text += "Thêm tk thành công!";
+
+        } else {
+            text += "Tk đã tồn tại!";
+        }
+
+    } else {
+        text += "Email không hợp lệ";
+    }
+    return text;
+}
+
+let email = prompt("Nhập email: ");
+
+let result = checkEmail(email);
+console.log(result);
